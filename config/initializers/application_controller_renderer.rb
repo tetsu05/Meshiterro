3c6359c6aaca9298279f815065dccc95d9824704
tetsu05 <tetsu051955@gmail.com>
2021-09-17 10:48:04 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bc6dba925b860a3dc69cdf28aac3f1085d4bf1a4f5d73e64f564bbceb9bba7eeb104813b14d11c4859499de5871f988573ea2dee56a915e48352c0f1d68d2c19'
